2f9586eb4312a0fede0e8e0c072798debd85f0aa
Irvin Olvera <irvinolve@gmail.com>
2020-11-16 23:44:21 +0000

    Cleaned

--- OOP_Coursework/PlayedMatch.java
+++ OOP_Coursework/PlayedMatch.java
@@ -1,11 +1,11 @@
 package OOP_Coursework;
 
 public class PlayedMatch {
-    private String date;
-    private String team_A_name;
-    private String team_B_name;
-    private int team_A_gaols;
-    private int team_B_gaols;
+    private final String date;
+    private final String team_A_name;
+    private final String team_B_name;
+    private final int team_A_gaols;
+    private final int team_B_gaols;
 
     PlayedMatch(String date, FootballClub team_A_name, int team_A_gaols, FootballClub team_B_name, int team_B_gaols){
         this.date = date;
@@ -15,18 +15,18 @@ public class PlayedMatch {
         this.team_B_gaols = team_B_gaols;
     }
     public String getDate(){
-        return this.date;
+        return date;
     }
     public String getTeam_A_name(){
-        return this.team_A_name;
+        return team_A_name;
     }
     public int getTeam_A_goals(){
-        return this.team_A_gaols;
+        return team_A_gaols;
     }
     public String getTeam_B_name(){
-        return this.team_B_name;
+        return team_B_name;
     }
     public int getTeam_B_goals(){
-        return this.team_B_gaols;
+        return team_B_gaols;
     }
 }
